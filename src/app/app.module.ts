9b2707d7a00bdf62ef99dda7ec1e30796eb687cc
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-12-06 02:30:51 -0500

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { ShippingComponent } from './shipping/shipping.component';
 import { FngAccordionComponent } from './fng-accordion/fng-accordion.component';
 import { GraphicDesignComponent } from './graphic-design/graphic-design.component';
 import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-lightbox.component';
+import { FooterComponent } from './graphic-design/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -68,7 +69,8 @@ import { PortfolioLightboxComponent } from './portfolio-lightbox/portfolio-light
     ShippingComponent,
     FngAccordionComponent,
     GraphicDesignComponent,
-    PortfolioLightboxComponent
+    PortfolioLightboxComponent,
+    FooterComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
