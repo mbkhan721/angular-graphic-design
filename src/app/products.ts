4e80f49ec6bf06809ec53b4527535469445c6a11
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-12-10 22:48:19 -0500

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -20,6 +20,13 @@ export const r1 = {
     "image": {
     "city": "https://i.pinimg.com/originals/90/7b/18/907b18cffbe0409cc15b577c87ab886e.jpg",
     "train":"https://i1.wp.com/ladobny.com/wp-content/uploads/2018/02/7-train.jpg?fit=750%2C555&ssl=1",
+    "food": "https://adventure.com/wp-content/uploads/2017/06/Various-Cantonese-dim-sum-in-Flushing-photo-credit-Erik-Trinidad.jpg",
+    "library": "https://onthegrid.city/imager/s3_amazonaws_com/onthegrid.city/assets/grid/queens/flushing/flushing-library/qnsmade-onthegrid-flushing-flushing-library-0002_299006722e285f47655d17d1c9136337.jpg",
+    "history": "https://i.pinimg.com/originals/80/ae/d3/80aed3b97c03a7cf834f03a01b51d46c.jpg",
+    "mall": "https://ds4.cityrealty.com/img/3944853d88603206e1abfcd917ecfa44189a28f0+w+h+0+60/sky-view-parc-tower-1-40-26-college-point-boulevard-01.jpg",
+    "globe": "https://media.timeout.com/images/100432609/630/472/image.jpg",
+    "park": "https://upload.wikimedia.org/wikipedia/commons/a/ad/VIEW_OF_UNISPHERE_FROM_SKYLINE_TOWER%2CFLUSHING%2CQUEENS%2CNY_-_panoramio.jpg",
+    "main": "https://queenspost.com/wp-content/uploads/2020/09/DOT.jpg"
   },
 
 }
