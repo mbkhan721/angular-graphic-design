8c8c7b0be69f09206eb440bccd88dab1ef341211
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-12-10 21:52:03 -0500

    1 file modified, 1 deleted

--- src/app/products.ts
+++ src/app/products.ts
@@ -16,6 +16,15 @@ export const products = [
   }
 ];
 
+export const r1 = {
+  "image": {
+    "city": "NYC",
+    "src": "https://i.pinimg.com/originals/90/7b/18/907b18cffbe0409cc15b577c87ab886e.jpg",
+    "train": "7 Train",
+    "https://i1.wp.com/ladobny.com/wp-content/uploads/2018/02/7-train.jpg?fit=750%2C555&ssl=1",
+  },
+
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
